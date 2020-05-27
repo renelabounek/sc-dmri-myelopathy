4e9765ff0bf13af8fac02aed65ae62e0a4449d2c
Rene Labounek <rene.labounek@gmail.com>
2020-05-27 15:16:31 -0500

    Update diff_analysis_bedpostx.sh

--- bash_scripts/diff_analysis_bedpostx.sh
+++ bash_scripts/diff_analysis_bedpostx.sh
@@ -17,6 +17,34 @@
 # Jan Valosek, Rene Labounek. fMRI laboratory Olomouc, Czech Republic, 2017-2020
 #
 #-----------------------------------------------------------------------------------------------------------
+#
+# Copyright 2016-2020 Rene Labounek (1,2,3,4), Jan Valosek (1,2) and Petr Hlustik (1,2)
+#
+# 1 - University Hospital Olomouc, Olomouc, Czech Republic
+# 2 - Palacky University Olomouc, Olomouc, Czech Republic
+# 3 - University Hospital Brno, Brno, Czech Republic 
+# 4 - University of Minnesota, Minneapolis, US
+#
+# This file is part of sc-dmri-myelopathy available at: https://github.com/renelabounek/sc-dmri-myelopathy
+#
+# Please, cite sc-dmri-myelopathy as:
+# Labounek R, Valosek J, Horak T, Svatkova A, Bednarik P, Vojtisek L, Horakova M, Nestrasil I,
+# Lenglet C, Cohen-Adad J, Bednarik J and Hlustik P. HARDI-ZOOMit protocol improves specificity
+# to microstructural changes in presymptomatic myelopathy. Scientific Reports [Revised; Under review]
+#
+# sc-dmri-myelopathy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sc-dmri-myelopathy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with sc-dmri-myelopathy.  If not, see <https://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------------------------------------------
 
 # Some global variable
 VERSION=01-03-2020
